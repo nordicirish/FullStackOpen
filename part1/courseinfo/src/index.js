d72ebfb2b4b1f8207525280a94c30a3f3a6074ec
Roger Graham <rogergraham44@gmail.com>
2020-09-08 18:04:51 +0300

    1.3: course information using objects
    1.3: course information step3
    Let's move forward to using objects in our application. Modify the variable definitions of the App component as follows and also refactor the application so that it still works:
    
    const App = () => {
      const course = 'Half Stack application development'
      const part1 = {
        name: 'Fundamentals of React',
        exercises: 10
      }
      const part2 = {
        name: 'Using props to pass data',
        exercises: 7
      }
      const part3 = {
        name: 'State of a component',
        exercises: 14
      }
    
      return (
        <div>
          ...
        </div>
      )
    }

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,64 +1,70 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// application refactored to use objects
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1 = {part1.name} part2 = {part2.name} part3 ={part3.name} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+    </div>
+  )
+}
+
   
   // Renders a single part using props
   const Part = (props) =>  {
-  console.log(props)
-   return (
-    <>
-      <p>{props.name} {props.exercise} </p>
-    </>
-   )
+    console.log(props)
+      return (
+        <>
+          <p>{props.part} {props.exercises} </p>
+        </>
+      )
  }
 
   const Header = (props) =>  {
     console.log(props)
-     return (
+      return (
        <>
        <h1>{props.course}</h1>
        </>
      )
    }
+   
    // Renders content using data passed as props 
   const Content = (props) => {
   console.log(props)
     return (
-    <div>
-      <Part name = {part1} exercise = {exercises1} /> 
-      <Part name = {part2} exercise = {exercises2} />
-      <Part name = {part3} exercise = {exercises3} />
-    </div>
-  )
+      <>
+        <Part part = {props.part1} exercises = {props.exercises1} /> 
+        <Part part = {props.part2} exercises = {props.exercises2} />
+        <Part part = {props.part3} exercises = {props.exercises3} />
+      </>
+    )
   }
   
   const Total = (props) => {
     console.log(props)
-    return (
-     
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-      
-    )
+      return (
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      )
   }
 
-  return (
-
-
-    <>
-      <Header course={course} />
-      <Content part1 = {part1} part2 = {part2} part3 ={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} /> 
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-    </>
-  )
-}
-
+  
 
 ReactDOM.render(<App />, document.getElementById('root'))
